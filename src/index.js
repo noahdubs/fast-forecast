0444aaa526aeff48dfb0697c51c1add33cbdbdd8
merchencer <merchencer@gmail.com>
2021-09-17 16:11:02 -0500

    added hash router

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import './index.css'
 
@@ -10,9 +10,9 @@ import store from './redux/store'
 
 ReactDOM.render(
     <Provider store={store}>
-        <BrowserRouter>
+        <HashRouter>
             <App />
-        </BrowserRouter>
+        </HashRouter>
     </Provider>, 
     document.getElementById('root')
 )
